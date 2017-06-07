dc3d767a051ff41102dbe7119ff05ff2b1faf48f
Catherine Karnyushina <catherine.nikolaev@gmail.com>
2017-06-07 14:09:43 +0300

    [angularjs.org] Controllers - Scope Inheritance Example

--- app/js/controllers/app.js
+++ app/js/controllers/app.js
@@ -1,10 +1,15 @@
-var myApp = angular.module('spicyApp2', []);
+var myApp = angular.module('scopeInheritance', []);
 
-myApp.controller('SpicyController', ['$scope', function($scope) {
-    $scope.customSpice = 'wasabi';
-    $scope.spice = 'very';
+myApp.controller('MainController', ['$scope', function($scope) {
+  $scope.timeOfDay = 'morning';
+  $scope.name = 'Nikki';
+}]);
 
-    $scope.spicy = function(spice) {
-        $scope.spice = spice;
-    };
+myApp.controller('ChildController', ['$scope', function($scope) {
+  $scope.name = 'Mattie';
+}]);
+
+myApp.controller('GrandChildController', ['$scope', function($scope) {
+  $scope.timeOfDay = 'evening';
+  $scope.name = 'Gingerbread Baby';
 }]);
