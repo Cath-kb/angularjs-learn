53f738fdf79b3e5af9dc6a4c0ca21b6f0d67ca44
Catherine Karnyushina <catherine.nikolaev@gmail.com>
2017-07-04 14:15:43 +0300

    [angularjs.org] Forms - Custom model update triggers

--- app/js/controllers/app.js
+++ app/js/controllers/app.js
@@ -1,19 +1,5 @@
-var myApp = angular.module('formExample', []);
+var myApp = angular.module('customTriggerExample', []);
 
 myApp.controller('ExampleController', ['$scope', function($scope) {
-  $scope.master = {};
-  
-  $scope.update = function(user) {
-    $scope.master = angular.copy(user);
-  };
-  
-  $scope.reset = function(form) {
-    if (form) {
-      form.$setPristine();
-      form.$setUntouched();
-    }
-    $scope.user = angular.copy($scope.master);
-  };
-  
-  $scope.reset();
+  $scope.user = {};
 }]);
